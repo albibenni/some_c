ca117c6a7c1615a094ee3ebad07dc7b3fc47ce8c
Alberto Benatti <albi.benni8@gmail.com>
2025-07-29 21:52:59 +0200

    sum and generic stuf

--- src/hello.c
+++ src/hello.c
@@ -4,7 +4,12 @@
  */
 #include <stdio.h>
 
+int sum(int x, int y) { return x + y; }
+
 int main(void) {
+  int s = sum(10, 5);
   printf("Hello, World!\n");
+  printf("sum: %d\n", s);
+
   return 0;
 }
