beda49f535f751605169262f7e03316f89a873cc
Alberto Benatti <albi.benni8@gmail.com>
2025-07-30 22:29:36 +0200

    cool bitop

--- src/just_sum_of_even.c
+++ src/just_sum_of_even.c
@@ -13,6 +13,15 @@ void do_sum(int *a, int *sum) {
   printf("Sum of arr is: %d\n", *sum);
 }
 
+void bit_op(int *a) {
+  int sum[2] = {0, 0};
+  for (int i = 0; i < 100; i++) {
+    sum[a[i] & 1] += a[i];
+  }
+
+  printf("Bit operator -> even sum: %d and odd sum: %d\n", sum[0], sum[1]);
+}
+
 int main(void) {
   int a[100];
   int sum = 0;
@@ -22,6 +31,9 @@ int main(void) {
   }
 
   do_sum(a, &sum);
+  int new_sum = 0;
+
+  bit_op(a);
 
   return 0;
 }
