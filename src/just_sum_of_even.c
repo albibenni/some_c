e9cc9210f3900817789b5d1b4dadf1e715d8cfc1
Alberto Benatti <albi.benni8@gmail.com>
2025-07-30 22:23:27 +0200

    same as before but with ref and pointers

--- src/just_sum_of_even.c
+++ src/just_sum_of_even.c
@@ -3,6 +3,15 @@
  * All rights reserved.
  */
 #include <stdio.h>
+void do_sum(int *a, int *sum) {
+  for (int i = 0; i < 100; i++) {
+    if (a[i] % 2 == 0) {
+      *sum += a[i];
+    }
+  }
+
+  printf("Sum of arr is: %d\n", *sum);
+}
 
 int main(void) {
   int a[100];
@@ -12,13 +21,7 @@ int main(void) {
     a[i] = i + 1;
   }
 
-  for (int i = 0; i < 100; i++) {
-    if (a[i] % 2 == 0) {
-      sum += a[i];
-    }
-  }
-
-  printf("Sum of arr is: %d\n", sum);
+  do_sum(a, &sum);
 
   return 0;
 }
